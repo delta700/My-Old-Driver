00db9743d50d9d52f19746f186dda0cc3867158d
Hxxven <142484325+hxxven10@users.noreply.github.com>
2024-03-10 20:36:52 +0900

    Delete Driver directory

--- Driver/Source File/Util/Util.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Define.h"
-
-namespace Util
-{
-	uint64_t FindPattern(uint64_t Address, uint64_t Len, BYTE* bMask, char* sMask);
-	uintptr_t GetModuleBase(const char* Name);
-}
