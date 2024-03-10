d33e93b242f579b6944261c65d8887a63db1b769
Hxxven <142484325+hxxven10@users.noreply.github.com>
2024-03-10 20:37:17 +0900

    Add files via upload

---
+++ Driver/Source File/Main.cpp
@@ -0,0 +1,67 @@
+#include "Memory/Memory.h"
+#include "Clean/Clean.h"
+
+/*
+* Credit: Hook https://www.unknowncheats.me/forum/anti-cheat-bypass/425352-driver-communication-using-data-ptr-called-function.html
+*
+* Author: Hxxven https://github.com/hxxven10
+*/
+
+void RequestThread(uintptr_t _Request)
+{
+    Request* Req = (Request*)_Request;
+
+    DebugPrint("[+] RequestThread Called \n");
+
+    if (Req->Request_Base != false)
+    {
+        Req->Base = Memory::GetBaseAddress(Req->Pid);
+    }
+
+    if (Req->Request_Read != false)
+    {
+        Memory::ReadProcessMemory(Req->Pid, (PVOID)Req->Address, Req->Read_Buffer, Req->Size);
+    }
+
+    if (Req->Request_Write != false)
+    {
+        Memory::WriteProcessMemory(Req->Pid, (PVOID)Req->Address, Req->Write_Buffer, Req->Size);
+    }
+
+}
+
+void CleanThread()
+{
+    DebugPrint("[+] CleanThread Called \n");
+
+    Clean::CleanUnloadedDrivers();
+}
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT Driver_Object, PUNICODE_STRING Registry_Path)
+{
+    UNREFERENCED_PARAMETER(Driver_Object);
+    UNREFERENCED_PARAMETER(Registry_Path);
+
+    DebugPrint("[+] Start Driver \n");
+
+    uintptr_t Base = Util::GetModuleBase(skCrypt("win32k.sys"));
+
+    if (!Base) {
+        DebugPrint("[-] Faild to Base \n");
+        return STATUS_FAILED_DRIVER_ENTRY;
+    }
+
+    DebugPrint("[+] Base: 0x%llX \n", Base);
+
+    uintptr_t Offset = Base + 0x65F08;
+
+    DebugPrint("[+] Offset: 0x%llX \n", Offset);
+
+    *reinterpret_cast<uintptr_t*>(Offset) = reinterpret_cast<uintptr_t>(&RequestThread);
+
+    CleanThread();
+
+    DebugPrint("[+] Driver SuccessFully Loaded \n");
+
+    return STATUS_SUCCESS;
+}
---
+++ Driver/Source File/Memory/Memory.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "../Util/Define.h"
+
+namespace Memory
+{
+	DWORD GetUserDirectoryTableBaseOffset();
+	uint64_t GetProcessDirectoryTableBase(PEPROCESS pProcess);
+
+	PVOID GetBaseAddress(int Pid);
+
+	NTSTATUS ReadPhysicalAddress(PVOID TargetAddress, PVOID Read_Buffer, SIZE_T Read_Size, SIZE_T* Read_Bytes);
+	NTSTATUS WritePhysicalAddress(PVOID TargetAddress, PVOID Write_Buffer, SIZE_T Write_Size, SIZE_T* Write_Bytes);
+
+	uint64_t TranslateLinearAddress(uint64_t DirectoryTableBase, uint64_t VirtualAddress);
+
+	NTSTATUS ReadProcessMemory(int Pid, PVOID Read_Address, PVOID Read_Buffer, SIZE_T Read_Size);
+	NTSTATUS WriteProcessMemory(int Pid, PVOID Write_Address, PVOID Write_Buffer, SIZE_T Write_Size);
+}
