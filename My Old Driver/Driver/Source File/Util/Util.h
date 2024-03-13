2b98270ab0d56b38f17158e26405c7edee02c3a6
Hxxven <142484325+hxxven10@users.noreply.github.com>
2024-03-14 00:12:44 +0900

    Update Util.h

--- My Old Driver/Driver/Source File/Util/Util.h
+++ My Old Driver/Driver/Source File/Util/Util.h
@@ -3,6 +3,6 @@
 
 namespace Util
 {
-	uint64_t FindPattern(uint64_t Address, uint64_t Len, BYTE* bMask, char* sMask);
+	uint64_t PatternScan(uint64_t Address, uint64_t Len, BYTE* bMask, char* sMask);
 	uintptr_t GetModuleBase(const char* Name);
 }
